8e370e43034dc055c7f6279b015b273287652353
SHRAVAN D A <79013703+VENOM8704@users.noreply.github.com>
2021-08-17 18:48:55 +0530

    Add files via upload

---
+++ shravan.js
@@ -0,0 +1,12 @@
+function makeRed(){
+    document.body.style.backgroundColor="red";
+}
+function makeBlue(){
+    document.body.style.backgroundColor="blue";
+}
+function makeGreen(){
+    document.body.style.backgroundColor="green";
+}
+function makeOrange(){
+    document.body.style.backgroundColor="orange";
+}
